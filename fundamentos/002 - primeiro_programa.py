29acb1b98c8dde646a807d86bf4708966e288fcf
jdrasini <luis.88.rasini@gmail.com>
2019-03-24 17:55:50 -0300

    teste.py & primeiro_programa.py

---
+++ fundamentos/002 - primeiro_programa.py
@@ -0,0 +1,19 @@
+# print('Primeiro Programa')
+
+# a = 1 \
+#         + 2
+# print(a)
+
+# help(print)
+
+# a = 1 + 2 + 3 + 4 + 5 + 6
+
+# a = 1 + 2 + 3
+# b = 4 + 5 + 6
+
+# print(a)
+# print(b)
+
+#
+
+
