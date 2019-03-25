13360c68bab3c9790560166cf04e1306024e7de2
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 17:27:04 -0300

    013 - operadores_ternarios.py

---
+++ fundamentos/013 - operadores_ternarios.py
@@ -0,0 +1,12 @@
+# Operadores ternarios - 013 - operadores_ternarios.py
+
+esta_chovendo = False
+
+situacao1 = 'Hoje estou com as roupas ' + ('secas.', 'molhadas.')[esta_chovendo]  # 'secas' = False / 'molhadas' = True
+
+situacao2 = 'Hoje estou com as roupas ' + ('molhadas.' if esta_chovendo else 'secas.')
+
+print(situacao1)
+print(situacao2)
+
+
