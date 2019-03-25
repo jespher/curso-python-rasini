315ab40670f400224f2719ea268572fe670b1e00
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 13:00:20 -0300

    007 - desafio_operadores_aritmeticos.py

---
+++ fundamentos/007 - desafio_operadores_aritmeticos.py
@@ -0,0 +1,9 @@
+salario = 3450.45
+despesas = 2456.2
+
+porcentagem_usada = despesas / salario * 100
+
+print('Utilizo do meu salário todo mês um percentual de: ' + (str(porcentagem_usada)) + ' %')
+
+
+
