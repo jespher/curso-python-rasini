34c375edc41a5f482d4d8d1ff5148ded85934325
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 13:55:57 -0300

    008 - operadores_relacionais.py

---
+++ fundamentos/008 - operadores_relacionais.py
@@ -0,0 +1,18 @@
+# Operadores Relacionais
+
+a = 3 > 4
+b = 4 >= 3
+c = 1 < 2
+d = 3 <= 1
+e = 3 != 2
+f = 3 == 3
+g = 2 == '2'
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
