1c342df5bce96ba7b030eae365b71c72b91153bc
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 15:44:04 -0300

    010 - operadores_logicos.py

---
+++ fundamentos/010 - operadores_logicos.py
@@ -0,0 +1,103 @@
+# Operadores Lógicos
+
+# a = True or False
+# print(a)
+
+# b = 7 != 3 and 2 > 3
+# print(b)
+
+# c = 3 == 4 or 15 < 18
+# print(c)
+
+# d = 3 * 4 == 12 and 12 < 20
+# print(d)
+
+# a = 5
+# b = 6
+# c = 8
+# d = 9
+
+# result = a < b and c > d
+# print(result)
+
+# result2 = a == b or d > d
+# print(result2)
+
+# TABELA verdade do AND
+
+# a = True and True
+# b = True and False
+# c = False and True
+# d = False and False
+
+# TABELA verdade de OR
+
+# e = True or True
+# f = True or False
+# g = False or True
+# h = False or False
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# print('')
+
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+# TABELA verdade de XOR(exclusivamente um ou outro)
+
+# A = True != True
+# B = True != False
+# C = False != True
+# D = False != False
+
+# print(A)
+# print(B)
+# print(C)
+# print(D)
+
+# TABELA verdade de negacao (unario)
+
+# a = not True
+# b = not False
+# c = not 0
+# d = not not -1
+# e = not not True
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+# bit a bit
+
+# w = True & True
+# print(w)
+
+# y = False | True
+# print(y)
+
+# z = True ^ True
+# print(z)
+
+# Um pouco de realidade
+
+saldo = 1000
+salario = 4000
+despesas = 2967
+
+meta = saldo > 0 and salario - despesas >= 0.2 * salario
+
+print(meta)
+
+
+
+
+
+
