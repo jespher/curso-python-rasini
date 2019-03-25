3ce14929dca1125c2bf166dddb9d74aaa16eb08e
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 09:40:02 -0300

    003 - tipos_basicos

---
+++ fundamentos/003 - tipos_basicos.py
@@ -0,0 +1,12 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print('Fale o que quiser')
+print("Entendeu? ")
+print('Você é ' + 3 * 'muito ' + 'legal!')
+# print(3 + '3') // desta forma não funciona
+print([1, 2, 3])
+print({'nome': 'Pedro', 'idade': 30})
+print(None)
+
+
