ebff6d2dec9797849b974fd9784bd19dbbb09459
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 12:35:06 -0300

    005 - comentários_código

---
+++ fundamentos/005 - comentarios_codigo.py
@@ -0,0 +1,20 @@
+# Minhas variaveis
+
+salario = 5200.55
+despesas = 2430.54
+
+"""
+Calcular quando irá sobrar no fim do mês # aspas duplas
+"""
+
+print(salario - despesas)
+
+# print("Fim")
+
+print('Fim de verdade')  # Aqui também pode ter comentário
+
+'''
+Calcular quando irá sobrar no fim do mês # aspas simples
+'''
+
+print('Agora SIMMM... O Fim de verdade')
