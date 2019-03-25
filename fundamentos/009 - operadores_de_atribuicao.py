a0b590762ff28fe8ec9005a9adf8689b575a0425
Jefferson <luis.88.rasini@gmail.com>
2019-03-25 14:16:43 -0300

    009 - operadores_de_atribuicao.py

---
+++ fundamentos/009 - operadores_de_atribuicao.py
@@ -0,0 +1,35 @@
+# Operadores de atribuição
+
+a = 3
+a = a + 7
+print(a)
+
+# or
+
+a = 3
+a += 7
+print(a)
+
+a = 2
+a -= 3
+print(a)
+
+a = 7
+a *= 3
+print(a)
+
+a = 8
+a /= 4
+print(a)
+
+a = 3
+a %= 4
+print(a)
+
+a = 2
+a **= 8
+print(a)
+
+a = 125
+a //= 5
+print(a)
