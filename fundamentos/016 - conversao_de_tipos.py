89d3e05aeebce3cc1807bdc95b3bf17f0c062d06
Jefferson <luis.88.rasini@gmail.com>
2019-03-26 15:38:44 -0300

    016 - conversao_de_tipos.py

---
+++ fundamentos/016 - conversao_de_tipos.py
@@ -0,0 +1,18 @@
+# Conversão de tipos
+
+# print(2 + 3)
+
+# print('2' + (str(3)))
+
+# print('2' + '3')
+
+a = 2
+b = '3'
+
+print(type(a))
+print(type(b))
+
+print(a + int(b))
+print(str(a) + b)
+
+
