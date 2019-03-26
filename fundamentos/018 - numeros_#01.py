8e8e73448c23ac02f647c67fc2ac3f6483ffeca9
Jefferson <luis.88.rasini@gmail.com>
2019-03-26 17:29:59 -0300

    018 - numeros_#01.py

---
+++ fundamentos/018 - numeros_#01.py
@@ -0,0 +1,27 @@
+# Tipos numéricos
+
+# print(dir(int))
+# print(dir(float))
+
+a = 3
+b = 5.8
+
+print(a / b)
+print(a + b)
+print(a * b)
+
+print(type(a))
+print(type(b))
+print(type(a - b))
+
+print(b.is_integer())
+
+print(5.0.is_integer())
+
+print(int.__add__(2, 3))
+
+print(float.__add__(2.5, 5.0))
+
+print(bin(10))
+
+
