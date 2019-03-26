5650f64b801384901603b5152801d773b2b7a49d
Jefferson <luis.88.rasini@gmail.com>
2019-03-26 10:34:45 -0300

    015 - builtins.py

---
+++ fundamentos/015 - builtins.py
@@ -0,0 +1,30 @@
+# builtins
+
+# type
+
+print(type(1))
+
+# print(__builtins__.type('Fala aí... '))
+
+# print(__builtins__.print(10 / 3))
+
+# print(help(dir()))
+
+print(__builtins__.help(__builtins__.dir))
+
+# a = 7
+
+# teste = 'QA'
+
+# nome = 'josé'
+
+# import math
+
+print(dir())
+
+print(dir([__builtins__]))
+
+
+
+
+
