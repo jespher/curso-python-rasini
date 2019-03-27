9e2576689f65b2202efe7ea7b51b6140f93800ff
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 13:47:01 -0300

    026 - Listas #03.py

---
+++ fundamentos/026 - Listas #03.py
@@ -0,0 +1,24 @@
+lista = ['Jeff', 'Dir', 'Sergio', 'Leiri', 'Denise', 'Ulderico']
+
+print(lista[1:3])
+
+print(lista[1:-1])
+
+print(lista[3:])
+
+print(lista[:-3])
+
+print(lista[:])
+
+print(lista[::2])
+
+print(lista[::-1])
+
+del lista[4]
+
+print(lista)
+
+del lista[1:]
+
+print(lista)
+
