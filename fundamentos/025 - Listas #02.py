14e230b651adfc5f7a86fc9f9ff085c9cf5e8c17
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 13:33:21 -0300

    025 - Listas #02.py

---
+++ fundamentos/025 - Listas #02.py
@@ -0,0 +1,19 @@
+# Acessar ítens da lista
+
+lista = [1, 5, 'Dirlayne', 'Jefferson', 3.1415]
+
+print(lista.index(3.1415))
+
+# print(lista.index(42))  # ValueError: 42 is not in list
+
+print(lista[2])
+
+print(lista[3])
+
+print(lista[-1])
+
+print('Jefferson' in lista)
+
+print(4 in lista)
+
+print('Dirlayne' not in lista)
