b3ebc370cfffe303ce2ade070cb50d66d7672d5b
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 11:32:28 -0300

    022 - Strings #03.py

---
+++ fundamentos/022 - Strings #03.py
@@ -0,0 +1,24 @@
+frase = 'Jefferson Luis Rasini é um nome completo'
+
+print('Ra' in frase)
+print('Ra' not in frase)
+
+print('completo' in frase)
+
+print('blabla' in frase)
+print('blabla' not in frase)
+
+print(len(frase))
+
+print(frase.lower())
+
+print(frase.upper())
+
+print(frase.split())
+
+print(frase.split('E'))
+
+print(frase.capitalize())
+
+print(frase.casefold())
+
