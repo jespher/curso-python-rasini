d28ea3ecd22a82e3551555dd51a0f310ec2e3010
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 15:49:32 -0300

    027 - Tuplas.py

---
+++ fundamentos/028 - Dicionários #01.py
@@ -0,0 +1,41 @@
+# Dicionário
+
+pessoa = {'nome': 'Prof(a). Denise',
+          'Idade': 38,
+          'cursos': ['Inglês', 'Português'],
+          'Alunos': ['Jefferson', 'Dirlayne']}
+
+print(type(pessoa))
+
+# print(dir(dict))
+
+print(len(pessoa))
+
+print(pessoa['nome'])
+
+print(pessoa['Idade'])
+
+print(pessoa['cursos'])
+
+print(pessoa['cursos'][0])
+print(pessoa['cursos'][1])
+
+print(pessoa['Alunos'])
+print(pessoa['Alunos'][0])
+print(pessoa['Alunos'][1])
+
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+print(pessoa.get('Idade'))
+print(pessoa.get('Alunos'))
+print(pessoa.get('Alunos')[0])
+print(pessoa.get('Alunos')[1])
+
+print(pessoa.get('cursos'))
+print(pessoa.get('cursos')[0])
+print(pessoa.get('cursos')[1])
+
+print(pessoa.get('tags', []))
+
+
