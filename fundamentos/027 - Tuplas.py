7f9bc3a209d70d93c364aed6845c72f44d900e8e
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 14:20:19 -0300

    027 - Tuplas.py

---
+++ fundamentos/027 - Tuplas.py
@@ -0,0 +1,22 @@
+# Tuplas
+
+tupla = tuple()
+
+print(type(tupla))
+
+tupla = 'um'
+
+print(tupla)
+
+cores = ('Azul', 'Azul', 'Verde', 'Amarelo', 'Vermelho', 'Branco')
+
+print(cores[::-1])
+print(cores[4])
+print(cores[-5])
+print(cores[2:])
+
+print(cores.index('Azul'))
+
+print(cores.count('Azul'))  # conta quantos tem na tupla
+
+print(len(cores))
