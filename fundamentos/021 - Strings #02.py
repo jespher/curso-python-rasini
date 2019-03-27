6c87373f3405820f1026c9108c310707728c856a
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 10:31:33 -0300

    021 - Strings #02.py

---
+++ fundamentos/021 - Strings #02.py
@@ -0,0 +1,81 @@
+nome = 'Jefferson Luis'
+numeros = '0123456789'
+# print(nome[10])
+# print(nome[-4])
+
+# print(nome[0])
+# print(nome[1])
+# print(nome[2])
+# print(nome[3])
+# print(nome[4])
+# print(nome[5])
+# print(nome[6])
+# print(nome[7])
+# print(nome[8])
+# print(nome[9])
+# print(nome[10])
+# print(nome[11])
+# print(nome[12])
+# print(nome[13])
+# print(nome[14]) está fora do range -> não existe essa posição
+
+# print('')
+
+# print(nome[-14])
+# print(nome[-13])
+# print(nome[-12])
+# print(nome[-11])
+# print(nome[-10])
+# print(nome[-9])
+# print(nome[-8])
+# print(nome[-7])
+# print(nome[-6])
+# print(nome[-5])
+# print(nome[-4])
+# print(nome[-3])
+# print(nome[-2])
+# print(nome[-1])
+
+print(nome[10:])
+
+print(nome[-4:])
+
+print(nome[:9])
+
+print(nome[:14])
+
+print(nome[:-10])
+
+print(nome[6:14])
+
+print(nome[::])
+
+print(numeros[::2])
+
+print(numeros[1::2])
+
+print(numeros[::-1])
+
+print(nome[::-1])  # nome invertido
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
