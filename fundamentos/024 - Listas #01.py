9f80682916bae252c2588e5877abf9572cf56134
Jefferson <luis.88.rasini@gmail.com>
2019-03-27 13:21:45 -0300

    024 - Listas #01.py

---
+++ fundamentos/024 - Listas #01.py
@@ -0,0 +1,25 @@
+# Listas
+
+lista = []
+
+print(type(lista))
+
+print(len(lista))
+
+lista.append(1)
+lista.append(3)
+lista.append(5)
+lista.append([7, 9, 11, 13, 15, 17])
+
+print(lista)
+
+new_list = [1, 3, 5, 'Jefferson', 'Luis']
+
+print(new_list)
+print(len(new_list))
+
+new_list.remove(5)
+print(new_list)
+
+new_list.reverse()
+print(new_list)
