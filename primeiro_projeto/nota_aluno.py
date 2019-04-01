43b99892bcbbb03ab5e26b685f4b63af5e979e77
Jefferson <luis.88.rasini@gmail.com>
2019-04-01 14:52:52 -0300

    nota_aluno.py

---
+++ primeiro_projeto/nota_aluno.py
@@ -0,0 +1,9 @@
+nota = 10
+if nota == 10:
+    print('Excelente, você foi aprovado com a nota máxima!!! ')
+elif nota >= 7:
+    print('Parabéns, você foi aprovado!!! ')
+elif nota >= 6:
+    print('Faltou muito pouco para sua aprovação. Você ficou de recuperação!!! ')
+else:
+    print('Você está reprovado!!')
