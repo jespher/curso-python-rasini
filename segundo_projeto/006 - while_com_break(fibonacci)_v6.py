05fb66dc7b8b03f9262da9be45ed757010d98af0
Jefferson <luis.88.rasini@gmail.com>
2019-04-03 14:30:58 -0300

    006 - while_com_break(fibonacci)_v6.py

---
+++ segundo_projeto/006 - while_com_break(fibonacci)_v6.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21...
+def fibonacci(quantidade):
+    resultado = [0, 1]
+    while True:
+        resultado.append(sum(resultado[-2:]))
+        if len(resultado) == quantidade:
+            break
+    return resultado
+
+
+if __name__ == '__main__':
+
+    for fib in fibonacci(20):
+        print(fib)
