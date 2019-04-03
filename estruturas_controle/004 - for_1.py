61d9a237efa0b99d2b12452cdae37f8c1276d1c9
Jefferson <luis.88.rasini@gmail.com>
2019-04-03 09:35:29 -0300

    004 - for_1.py

---
+++ estruturas_controle/004 - for_1.py
@@ -0,0 +1,17 @@
+# primeira forma de interpolar
+# for i in range(0, 11):
+#     print('i = {}'.format(i))
+#
+# for k in range(6):
+#     print('k = {}'.format(k))
+
+# segunda forma de interpolar
+# for j in range(11):
+#     print(f'j = {j}')
+#
+# for numero in range(0, 26):
+#     print(f'numero = {numero}')
+
+for x in range(2, 10):
+    for y in range(1, 11):
+        print(f'{x} * {y} = {x * y}')
