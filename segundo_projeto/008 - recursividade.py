898fea59ceea22838d0fbca2b6d074d34789b2ff
Jefferson <luis.88.rasini@gmail.com>
2019-04-03 14:53:36 -0300

    008 - recursividade.py

---
+++ segundo_projeto/008 - recursividade.py
@@ -0,0 +1,15 @@
+# def imprimir(maximo, atual):
+#     if atual >= maximo:
+#         return
+#     print(atual)
+#     imprimir(maximo, atual + 1)
+#
+# imprimir(10, 1)
+
+def imprimir(maximo, atual):
+    if atual < maximo:
+        print(atual)
+        imprimir(maximo, atual + 1)
+
+
+imprimir(100, 1)
