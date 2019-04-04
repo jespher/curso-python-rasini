5a8b5bb5ab4851b3b24ec17dce231d8b78fea294
Jefferson <luis.88.rasini@gmail.com>
2019-04-04 10:40:05 -0300

    ajustes

--- segundo_projeto/002 - usando_while_infinito(fibonaci)_v2.py
+++ segundo_projeto/002 - usando_while_infinito(fibonaci)_v2.py
@@ -1,14 +1,13 @@
 #!/usr/local/bin/python3
 
-
 # 0, 1, 1, 2, 3, 5, 8, 13, 21...
 def fibonacci(limite):
     penultimo = 0
     ultimo = 1
-    print(f'{penultimo}, {ultimo}', end=',')
+    print(f'{penultimo}, {ultimo}', end='/')
     while ultimo < limite:
         proximo = penultimo + ultimo
-        print(proximo, end=',')
+        print(proximo, end='/')
         penultimo = ultimo
         ultimo = proximo
 
