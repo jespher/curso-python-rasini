c1ea3ca274ab625b444d58d49fd98247ff5f5907
Jefferson <luis.88.rasini@gmail.com>
2019-04-04 12:11:55 -0300

    002 - comprehension_v2.py

---
+++ comprehension/002 - comprehension_v2.py
@@ -0,0 +1,10 @@
+# [ expressao for item in list if condicional ]
+dobros_dos_pares = [i * 2 for i in range(10) if i % 2 == 0]
+print(dobros_dos_pares)
+
+# versao "normal"
+dobros_dos_pares = []
+for i in range(10):
+    if i % 2 == 0:
+        dobros_dos_pares.append(i * 2)
+print(dobros_dos_pares)
