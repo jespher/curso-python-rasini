6385841bec66a85a13608aaa81f98dd7f0154879
Jefferson <luis.88.rasini@gmail.com>
2019-04-04 12:03:11 -0300

    # versao normal

--- comprehension/001 - comprehension_v1.py
+++ comprehension/001 - comprehension_v1.py
@@ -1,3 +1,10 @@
 # [ expressao for item in list if condicional ]
 dobros = [i * 2 for i in range(1, 11)]
 print(dobros)
+
+
+# versao normal
+dobros = []
+for i in range(1, 11):
+    dobros.append(i * 2)
+print(dobros)
