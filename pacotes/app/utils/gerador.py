b5c62eab6268e4ea475425698c409c5da5f03597
jdrasini <luis.88.rasini@gmail.com>
2019-04-06 14:39:32 -0300

    pacotes parte 3

---
+++ pacotes/app/utils/gerador.py
@@ -0,0 +1,5 @@
+from random import choice
+
+
+def novo_nome():
+    return choice(['Jeff', 'Dir', 'Mel', 'Tchui', 'Derico', 'Denise', 'Sergio', 'Leiri'])
---
+++ pacotes/pacote_v5.py
@@ -0,0 +1,4 @@
+from calc import soma, subtracao
+
+print('Soma: ', soma(2, 3))
+print('Subtracao: ', subtracao(5, 2))
