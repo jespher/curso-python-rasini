e79029ef8351e4bfde65facf97ea3b5f7f4dc688
Jefferson <luis.88.rasini@gmail.com>
2019-04-11 10:31:08 -0300

    contador_objetos.py

---
+++ poo_avancado/contador_objetos.py
@@ -0,0 +1,16 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+
+class ClasseSimples:
+    contador = 0
+
+    def __init__(self):
+        self.contar()
+
+    @classmethod
+    def contar(cls):
+        cls.contador += 1
+
+
+if __name__ == '__main__':
+    lista = [ClasseSimples(), ClasseSimples(), ClasseSimples()]
+    print(ClasseSimples.contador)  # esperado 2
