d804f704821c6ec656b50ddb6ef1d18c7d6eecc1
Jefferson <luis.88.rasini@gmail.com>
2019-04-11 11:18:15 -0300

    funcao_alta_ordem.py

---
+++ programacao_funcional/funcao_alta_ordem.py
@@ -0,0 +1,12 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+# coding=utf8
+from funcao_primeira_classe import dobro, quadrado
+
+def processar(titulo, lista, funcao):
+    print(f'Processando: {titulo}')
+    for i in lista:
+        print(i, '=>', funcao(i))
+
+if __name__ == '__main__':
+    processar('Dobros de 1 a 10', range(1, 11), dobro)
+    processar('Quadrados de 1 a 10', range(1, 11), quadrado)
