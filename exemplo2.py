4f5323e553934dbbf0328f3af2c530c41e224141
Jefferson <luis.88.rasini@gmail.com>
2019-04-11 17:44:47 -0300

    exemplo2.py

---
+++ exemplo2.py
@@ -0,0 +1,17 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+# coding=utf8
+
+from calendar import mdays, month_name
+
+print(month_name[1], mdays[1])
+print(month_name[2], mdays[2])
+print(month_name[3], mdays[3])
+print(month_name[4], mdays[4])
+print(month_name[5], mdays[5])
+print(month_name[6], mdays[6])
+print(month_name[7], mdays[7])
+print(month_name[8], mdays[8])
+print(month_name[9], mdays[9])
+print(month_name[10], mdays[10])
+print(month_name[11], mdays[11])
+print(month_name[12], mdays[12])
