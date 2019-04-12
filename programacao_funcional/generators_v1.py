034a4af094c93c6aa7301918f09d098275046d8f
Jefferson <luis.88.rasini@gmail.com>
2019-04-12 12:40:42 -0300

    generators_v1.py

---
+++ programacao_funcional/generators_v1.py
@@ -0,0 +1,18 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+# coding=utf8
+
+def cores_arco_iris():
+    yield 'vermelho'
+    yield 'laranja'
+    yield 'amarelo'
+    yield 'verde'
+    yield 'azul'
+    yield 'índigo'
+    yield 'violeta'
+
+
+if __name__ == '__main__':
+    generator = cores_arco_iris()
+    print(type(generator))
+    while True:
+        print(next(generator))
