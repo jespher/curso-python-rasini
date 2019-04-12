3cc00c3f3df855522d00c9b8eaf54a48ceff3823
Jefferson <luis.88.rasini@gmail.com>
2019-04-12 12:49:25 -0300

    generators_v3.py

---
+++ programacao_funcional/generators_v3.py
@@ -0,0 +1,30 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+# coding=utf8
+
+def sequence():
+    num = 0
+    while True:
+        num += 1
+        yield num
+
+
+if __name__ == '__main__':
+    seq = sequence()
+
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
