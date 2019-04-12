41193e74328942024b45401ea42deaf8143b163f
Jefferson <luis.88.rasini@gmail.com>
2019-04-12 12:28:04 -0300

    funcoes_imutabilidade_v1.py

---
+++ programacao_funcional/funcoes_imutabilidade_v1.py
@@ -0,0 +1,21 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+# coding=utf8
+
+from functools import reduce
+from operator import add
+
+valores = [30, 10, 25, 55, 5, 8, 965, 87, 99, 65, 38]
+
+print(sorted(valores))
+print(valores)
+
+# valores.sort()
+# print(valores)
+
+print(min(valores))
+print(max(valores))
+print(sum(valores))
+print(reduce(add, valores))
+print(reversed(valores))
+print(list(reversed(valores)))
+print(valores)
