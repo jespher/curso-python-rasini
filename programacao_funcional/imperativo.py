bd09040df10703aa0b24da6d81217e4353dfbe19
Jefferson <luis.88.rasini@gmail.com>
2019-04-12 10:47:17 -0300

    imperativo.py

---
+++ programacao_funcional/imperativo.py
@@ -0,0 +1,15 @@
+#!C:\Users\Denis\AppData\Local\Programs\Python\Python37
+# coding=utf8
+
+from locale import setlocale, LC_ALL
+from calendar import mdays, month_name
+
+# Português do Brasil
+setlocale(LC_ALL, 'pt_BR')
+
+# Listar todos os meses do ano com 31 dias
+print('Meses com 31 dias:')
+for mes in range(1, 13):
+    if mdays[mes] == 31:
+        print(f'- {month_name[mes]}')
+
